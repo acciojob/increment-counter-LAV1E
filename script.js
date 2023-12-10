82d47b933b7d2460875f74256104495f92489155
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-10 12:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementById('incrementBtn').addEventListener('click', function() {
+    let counter = document.getElementById('counter');
+    let currentValue = Number(counter.innerText);
+    alert("The un-incremented value is: " + currentValue);
+    counter.innerText = currentValue + 1;
+});
