39bdbe8db6c0c72ae1e4a03f16e47254a2bcb818
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-12 10:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 document.getElementById('incrementBtn').addEventListener('click', function() {
     let counter = document.getElementById('counter');
     let currentValue = Number(counter.innerText);
-    alert("localhost:3000" + currentValue);
+    alert("localhost:300" + currentValue);
     counter.innerText = currentValue + 1;
 });
