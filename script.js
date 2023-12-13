9c2bf3b41a5742f3c96867b2e4bfffdd3638aacb
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-13 10:53:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 //your JS code here. If required.
-document.getElementById('incrementBtn').addEventListener('click', function() {
-    let counter = document.getElementById('counter');
-    let currentValue = Number(counter.innerText);
-    alert("localhost:300" + currentValue);
-    counter.innerText = currentValue + 1;
-});
+        function incrementCounter() {
+            // Get the current value of the counter
+            var counterElement = document.getElementById("counter");
+            var currentCounterValue = parseInt(counterElement.innerText);
+
+            // Display an alert with the un-incremented value
+            alert("Un-incremented value: " + currentCounterValue);
+
+            // Increment the counter value
+            var newCounterValue = currentCounterValue + 1;
+
+            // Update the counter element with the new value
+            counterElement.innerText = newCounterValue;
+        }
